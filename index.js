a0879989b60d0b89acfe76afd6754846cee74fb8
이혜진 <4mikemendes@gmail.com>
2021-08-12 10:33:16 +0200

    create const postFive

--- index.js
+++ index.js
@@ -15,4 +15,7 @@
 
  fetch("https://apis.scrimba.com/jsonplaceholder/posts")
  .then(res => res.json())
- .then(data => console.log(data.slice(0,5)))
+ .then(data => {
+const postsFive = data.slice(0,5)
+console.log(postsFive)
+ })
